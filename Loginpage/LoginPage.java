f556987346da5ee5bec1c511cb5358c1e7c2eacb
Sam <58564925+sambhavvinayak@users.noreply.github.com>
2023-01-22 11:10:24 -0500

    Update LoginPage.java

--- Loginpage/LoginPage.java
+++ Loginpage/LoginPage.java
@@ -1,49 +1,85 @@
 
-
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 import javax.swing.*;
 
 public class LoginPage extends JFrame {
+    private Cursor curT= new Cursor(Cursor.TEXT_CURSOR);
+    private Cursor curP = new Cursor(Cursor.HAND_CURSOR);
+    private Font font = new Font("Arial",Font.BOLD,15);
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JLabel usernameLabel;
     private JLabel passwordLabel;
     private JButton loginButton;
-    private JButton Back;
+    private JButton Back , Show;
     public String user , pass;
 
     public LoginPage() {
         setTitle/*we ca write here super also*/("Login Page");
-        setLayout(new FlowLayout());
+        Container c = getContentPane();
+        c.setLayout(null);
 
         usernameLabel = new JLabel("Username: ");
-        add(usernameLabel);
+        usernameLabel.setForeground(Color.MAGENTA);
+        usernameLabel.setBounds(10,40,100, 20);
+        usernameLabel.setFont(font);
+        usernameLabel.setCursor(curT);
+        c.add(usernameLabel);
 
         usernameField = new JTextField(20/*this is for a size defining */);
-        add(usernameField);
+        usernameField.setBounds(120,40,200, 20);
+        usernameField.setFont(font);
+        usernameField.setForeground(Color.RED);
+        c.add(usernameField);
 
         passwordLabel = new JLabel("Password: ");
-        add(passwordLabel);
+        passwordLabel.setBounds(10,80,100, 20);
+        passwordLabel.setForeground(Color.MAGENTA);
+        passwordLabel.setCursor(curT);
+        passwordLabel.setFont(font);
+        c.add(passwordLabel);
 
         passwordField = new JPasswordField(20);
-        add(passwordField);
+        passwordField.setBounds(120,80,200, 20);
+        passwordField.setForeground(Color.RED);
+        c.add(passwordField);
+        passwordField.setFont(font);
 
         loginButton = new JButton("Login");
+        loginButton.setBounds(85,150,100, 30);
+        loginButton.setFont(font);
+        loginButton.setCursor(curP);
+        c.add(loginButton);
+
         Back = new JButton("Back");
-        add(loginButton);
-        add(Back);
+        Back.setBounds(200,150,100, 30);
+        Back.setCursor(curP);
+        Back.setFont(font);
+        c.add(Back);
+
+        Show = new JButton("^");
+        Show.setBounds(190,100,50, 20);
+        c.add(Show);
+        Show.setCursor(curP);
+
+
+        Show.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                passwordField.setEchoChar((char)0);
+                Show.setEnabled(false);
+            }
+        });
 
         Back.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 Signup sgn = new Signup();
-                setVisible(false);
                 sgn.setVisible(true);
                 dispose();
+                sgn.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             }
         });
 
@@ -87,8 +123,12 @@ public class LoginPage extends JFrame {
                 }
             }
         });
+
+
+        setLocation(500, 100);
         setSize(350, 500);
         setVisible(true);
+
     }
 
     public static void main(String[] args) {
